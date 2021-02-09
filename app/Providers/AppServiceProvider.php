e3e73d93d08879d1f8a5585690956e430978b034
gamal khaled <jamal2014.1440@gmail.com>
2021-02-09 02:56:46 +0200

    add guard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\AdminController;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Actions\AttemptToAuthenticate;
+use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +18,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->when([
+            AdminController::class,
+            AttemptToAuthenticate::class,
+            RedirectIfTwoFactorAuthenticatable::class
+        ])->needs(StatefulGuard::class)->give(function(){
+            return Auth::guard('Admins');
+        });
     }
 
     /**
